beeba5d265da4593821057f98ba92d96c29dcecc
Jarad DeLorenzo <jaradd@gmail.com>
2012-08-05 13:40:47 -0400

    Added product admin routes. Added product admin toolbar

--- src/ElleOL/AdminBundle/Controller/DefaultController.php
+++ src/ElleOL/AdminBundle/Controller/DefaultController.php
@@ -3,11 +3,17 @@
 namespace ElleOL\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ElleOL\SiteBundle\Entity\Product as Product;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ElleOLAdminBundle:Default:index.html.twig');
+    	$em = $this->getDoctrine()->getEntityManager();
+    	$repo = $this->getDoctrine()->getRepository("ElleOLSiteBundle:Product");
+
+    	$products = $repo->findAll();
+
+        return $this->render('ElleOLAdminBundle:Default:index.html.twig', array("products" => $products));
     }
 }
